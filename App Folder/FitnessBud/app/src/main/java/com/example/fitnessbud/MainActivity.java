65a4c39f37e55a420e2ab19fe82610577aa52b80
TheodoreTsimiklis <70531739+TheodoreTsimiklis@users.noreply.github.com>
2022-04-17 17:22:36 -0400

    register-login and basic profile

---
+++ App Folder/FitnessBud/app/src/main/java/com/example/fitnessbud/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.fitnessbud;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register, forgotPassword;
+    private EditText email, password;
+    private Button login;
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(this);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        forgotPassword = findViewById(R.id.forgotPassword);
+        forgotPassword.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+            case R.id.login:
+                userLogin();
+                break;
+            case R.id.forgotPassword:
+                startActivity(new Intent(this, ForgotPassword.class));
+        }
+    }
+
+    private void userLogin() {
+        String userEmail = email.getText().toString().trim();
+        String userPass = password.getText().toString().trim();
+
+        if (userEmail.isEmpty()) {
+            email.setError("Email is required!");
+            email.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+            email.setError("Please provide a valid email");
+            email.requestFocus();
+            return;
+        }
+
+        if (userPass.isEmpty()) {
+            password.setError("Password is required!");
+            password.requestFocus();
+            return;
+        }
+
+        if (userPass.length() < 6) {
+            password.setError("Password should be at least 6 characters long!");
+            password.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(userEmail,userPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                    if (user.isEmailVerified()) {
+                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                    }else {
+                        user.sendEmailVerification();
+                        Toast.makeText(MainActivity.this, "Check email to verify account!", Toast.LENGTH_LONG).show();
+                    }
+
+                }else {
+                    Toast.makeText(MainActivity.this,"Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
